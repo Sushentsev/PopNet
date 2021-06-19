be5b1572081472bf052003c90bf8ef4a747f3ccb
grazder <grazder@yandex.ru>
2021-06-19 14:38:58 +0300

    arg parser

--- inference/gpt_inference.py
+++ inference/gpt_inference.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     parser.add_argument("--device", type=str, help="device - cuda / cpu", default=DEVICE)
     args = parser.parse_args()
 
-    inferencer = (args.song_name, args.device)
+    inferencer = (args.model_path, args.device)
     print(inferencer.predict(args.song_name))
